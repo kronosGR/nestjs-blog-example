60e6030a84cf03cc997ad3fe5323d9d5b194c744
kronosGR <geo.elgeo@gmail.com>
2025-05-08 11:34:56 +0200

    added and configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,18 @@ async function bootstrap() {
       transform: true, // auto transform to DTO after validation
     }),
   );
+
+  // <-- swagger config start -->
+  const config = new DocumentBuilder()
+    .setVersion('1.0')
+    .setTitle('NestJS API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // <-- swagger config end -->
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
